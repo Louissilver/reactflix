fb01c874fd60ae5dc5b826e0db9fd23ad747fd01
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2020-08-01 00:51:39 -0300

    Versão final 2.0

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import CadastroCategoria from './pages/cadastro/Categoria';
 const Pagina404 = () => (
   <div style={{ display: 'flex', alignItems: 'center', backgroundColor:'black', justifyContent: 'center', flexDirection: 'column', height: '120vh', marginTop: '-20vh', padding:'0' }}>
     // eslint-disable-next-line no-trailing-spaces
-    <h1 style={{textAlign: 'center', color: 'white'}}>Ops, parece que a cabeça de Ned Stark não está por aqui,<br /> acho melhor você procurar lá na <a href="/">home</a>.</h1>    
+    <h1 style={{textAlign: 'center', color: 'white'}}>Erro 404<br />Ops, parece que a cabeça de Ned Stark não está por aqui,<br /> acho melhor você procurar lá na <a href="/">home</a>.</h1>    
       
-      <iframe width="853" height="480" src="https://www.youtube.com/embed/iAPLUIVe0OI?autoplay=1;mute=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="853" height="480" src="https://www.youtube.com/embed/s7L2PVdrb_8?autoplay=1;mute=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 )
 
