c6eb34d4d0653fff8386e805530cbc4d324014bb
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2020-08-02 21:34:34 -0300

    Link github no footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ function Footer() {
       <a href="https://www.alura.com.br/">
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
-      <p>&copy; 2020 | By @Louissilver</p>
+      <p>&copy; 2020 | By <a href="https://github.com/Louissilver">@Louissilver</a></p>
       <p>
         Orgulhosamente criado durante a
         {' '}
